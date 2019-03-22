e071c3a1b445e7989ce6b226be758e94be4bf3e1
Mpho Vanencia Thebeyagae <Mpho@explore-datascience.net>
2019-03-22 10:36:58 +0200

    First commit

---
+++ gee/recursion.py
@@ -0,0 +1,43 @@
+def sum_array(array):
+
+    """
+
+    """
+    if array:
+        return array[0] + sum_array(array[1:])
+    else:
+        return 0
+
+
+
+def fibonacci(n):
+
+    """
+    Calculate nth term in fibonacci sequence
+
+    Args:
+        n (int): nth term in fibonacci sequence to calculate
+
+    Returns:
+        int: nth term of fibonacci sequence,
+             equal to sum of previous two terms
+
+
+    """
+
+    if n <= 1:
+        return n
+
+    else:
+        return fibonacci(n - 1) + fibonacci(n - 2)
+
+
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
+
+
+        def reversed_string(word):
+    return word[::-1]
